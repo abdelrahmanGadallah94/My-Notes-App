00563d73aca2b4e2d81c01f7261b5ccc96ecea65
abdelrahmanGadallah94 <abhg94@yahoo.com>
2023-01-04 03:19:08 +0200

    format date

--- lib/control/add_notes_method.dart
+++ lib/control/add_notes_method.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_notes_app/cubit/notes_cubit/notes_cubit.dart';
-
 import '../cubit/add_note_cubit/add_notes_cubit.dart';
 import '../model/notes_model.dart';
 import '../views/settings/app_colors.dart';
+import 'package:intl/intl.dart';
 
 void addNotesMethod(String? title, String? subtitle, BuildContext context,
     formKey, autoValidateMode,setState) {
   if (formKey.currentState!.validate()) {
     formKey.currentState!.save();
     DateTime currentDate = DateTime.now();
+    String formattedDate = DateFormat.Hm().format(currentDate);
     NotesModel note = NotesModel(
         title: title!,
         subtitle: subtitle!,
-        date: DateTime.now().toString(),
+        date: formattedDate,
         colors: AppColors.kCustomCardColor.value);
     BlocProvider.of<AddNotesCubit>(context).addNotesMethod(note);
   } else {
